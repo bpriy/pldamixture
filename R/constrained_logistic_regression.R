a10ca3af65e9b1fe6879218865344e9f406d8937
bpriy <98420978+bpriy@users.noreply.github.com>
2023-12-13 07:01:15 -0500

    constrained logistic regression update

--- R/constrained_logistic_regression.R
+++ R/constrained_logistic_regression.R
@@ -6,6 +6,8 @@ constrained_logistic_regression <- function(X, y, bound, cmaxiter){
   #mean(X %*% beta)
   barx <- colMeans(X)
 
+  # parameter for back-tracking line search
+  tau <- 0.1
 
   nloglik_logistic <- function(mu){
 
@@ -75,33 +77,58 @@ constrained_logistic_regression <- function(X, y, bound, cmaxiter){
       theta <- betanew_theta[d+1]
     }
     # linesearch
-    foo <- function(gamma){
-      betagamma <- (1-gamma) * betacur + gamma * betanew
-      mu <- plogis(X %*% betagamma)
-      nloglik_logistic(mu)
-    }
+
+    #foo <- function(gamma){
+    #  betagamma <- (1-gamma) * betacur + gamma * betanew
+    #  mu <- plogis(X %*% betagamma)
+    #  nloglik_logistic(mu)
+    #}
     #gammastar <- optimize(foo, interval = c(0,1))$minimum
     m <- 0
-    repeat{
-      if(is(try(optimize(foo, interval = c(0,2^-m))$minimum, silent = TRUE),"try-error")){
-        m <- m + 1
-      } else {
-        gammastar <- optimize(foo, interval = c(0,2^-m))$minimum
-        break}
-      if(2^(-m) < tol) {
-        gammastar <- optimize(foo, interval = c(0,2^-m))$minimum
-        break}
+
+    # inexact back-tracking line search via Armijo rule
+    upd <- betanew - betacur
+    munew <- plogis(X %*% betanew)
+    objnew <- nloglik_logistic(munew)
+
+    while( (objs[iter] - objnew) < tau * (1/2)^m * sum(upd * (-gr))){
+      m <- m + 1
+      if((1/2)^m < tol^2){
+        betanew <- betacur
+        break
+      }
+
+      else{
+        betanew <- betacur + upd * (1/2)^m
+        munew <- plogis(X %*% betanew)
+        objnew <- nloglik_logistic(munew)
+      }
     }
-    betacur <- (1-gammastar) * betacur + gammastar * drop(betanew)
+
+    betacur <- betanew
+
+    mu <- munew
+    iter <- iter+1
+    objs[iter] <- objnew
+
+    #repeat{
+
+    #if(is(try(optimize(foo, interval = c(0,2^-m))$minimum, silent = TRUE),"try-error")){
+    #  m <- m + 1
+    #} else {
+    #  gammastar <- optimize(foo, interval = c(0,2^-m))$minimum
+    #  break}
+    #if(2^(-m) < tol) {
+    #  gammastar <- optimize(foo, interval = c(0,2^-m))$minimum
+    #  break}
+    #}
+    #betacur <- (1-gammastar) * betacur + gammastar * drop(betanew)
     #if(is.matrix(betacur)){
     #    browser()
     #}
-    mu <- plogis(X %*% betacur)
-    iter <- iter+1
-    objs[iter] <- nloglik_logistic(mu)
+
 
     if(iter >= 2){
-      if(is.na(objs[iter])) browser()
       if(objs[iter-1] - objs[iter] < tol)
         break
     }
