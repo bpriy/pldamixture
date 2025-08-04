adb79244ec2628fc78e3f8616ac938c27f196c07
bpriy <98420978+bpriy@users.noreply.github.com>
2025-08-04 14:45:57 -0400

    Update fit_mixture_cox.R

--- R/fit_mixture_cox.R
+++ R/fit_mixture_cox.R
@@ -111,7 +111,7 @@ fit_mixture_cox <- function(formula, data, family,
     gammacur <- "default"
   } else {
     if(!missing(mrate)){
-      if(sum(Delta == 1) == n){
+      if(identical(Delta, matrix(nrow = n, ncol = 1, data = 1))){
         gammacur <- rep(-logitbound, ncol(Delta))
       } else {
         gammacur <- c(min(logitbound, 0), rep(0, ncol(Delta)-1))
@@ -185,7 +185,7 @@ fit_mixture_cox <- function(formula, data, family,
     }
 
     if(!missing(mrate)){
-      if(sum(Delta == 1) == n){
+      if(identical(Delta, matrix(nrow = n, ncol = 1, data = 1))){
         glm_h <- glm(pcur[!is_flagged] ~ Delta[!is_flagged,] - 1, family = quasibinomial)
         gammacur <- max(coef(glm_h), -logitbound)
       }
